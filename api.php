68db6a81738f8fec6aa88f8ee0816c694424498d
luckysher <luckyshera95@gmail.com>
2018-04-09 12:11:17 +0530

    updated api and added import for server file

--- api.php
+++ api.php
@@ -1,7 +1,15 @@
 <?php
+   // <!-- including Server file  -->
+   include 'server/server.php';
+   // <!-- including Server file  -->
 
-   $test = [
-            array("name" => "api", "language": "PHP", "uses": "configuration server")];
+   //$test = [
+   //         array('name' => 'api', 'language' => 'PHP', 'uses'=> 'configuration server')];
 
-   exit(json_decode($test));
+   $server = new Server;
+
+   $json_data = server->process_request($_POST, $_GET);
+
+   exit(json_encode($json_data));
+
 ?>
