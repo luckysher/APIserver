9ce893d2cd40ec0ddea72647c9be41c024f61dcf
luckysher <luckyshera95@gmail.com>
2018-04-09 12:11:50 +0530

    added server for processing requests

--- server/server.php
+++ server/server.php
@@ -6,6 +6,18 @@ class Server{
     public function __contruct(){
      $this -> appname = 'API Server';
     }
+
+    public function process_request(post, get){
+        $json = [];
+        if(post){
+           $json = [array('method' => 'post')];
+        }
+        elseif(get){
+                $json = [array('method' => 'get')];
+        }else {
+                $json = [array('Request error' => 'invalid request')];
+            }
+       return $json;
 }
 
 ?>
