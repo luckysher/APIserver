676c993699f75cc3fe53dd17be75228aeb542ad1
luckysher <luckyshera95@gmail.com>
2018-04-11 00:28:52 +0530

    added method for handling getting and setting config

--- controller/controller.php
+++ controller/controller.php
@@ -1,22 +1,39 @@
 <?php
  include 'gameConf/gameConf.php'
 
+
+// API controller
 class Controller{
     public $contname;
     public $grh;
 
+    // constructor for controller
     public function __contruct(){
        $this -> contname = 'API Controller';
        $this -> grh = new GameConfig;
     }
 
+    // Method for getting configurations
+    public function getConfigurations($qs){
+
+    }
+
+    // Method for setting configurations
+    public function setConfigurations($qs){
+
+    }
+
+
+    // Request Mapping
     public function process_request($s){
         $json = [];
-        $path_info = $s['PATH_INFO'];
+        $pi = $s['PATH_INFO'];
 
-        switch($path_info){
+        switch($pi){
             case '/getconfig':
                 echo '<h3 style="color:red;">Getting config:</h3>';
+                $qs = $s['QUERY_STRING'];
+                getConfigurations($qs);
                 break;
             case '/setconfig':
                 echo '<h3 style="color:red;">setting config:</h3>';
