8fd6ab36e4d584cce442318df677f73ddd3eafd3
luckysher <luckyshera95@gmail.com>
2018-04-19 11:02:47 +0530

    added config manager

---
+++ config/configManager.php
@@ -0,0 +1,13 @@
+
+// config manager class for fetching and
+// setting configurations in .json file
+
+class ConfigManager{
+
+    public function __construct($cfile){
+        // select appropriate config file user provided/default
+
+        endif:
+
+    }
+}
