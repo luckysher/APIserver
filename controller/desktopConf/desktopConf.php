5f8dc6737634823ccd5ffea3ca92340429355744
luckysher <luckyshera95@gmail.com>
2018-04-27 12:15:36 +0530

    added method for getting desktop conf

--- controller/desktopConf/desktopConf.php
+++ controller/desktopConf/desktopConf.php
@@ -3,6 +3,17 @@
 include 'config/configManager.php';
 
 class DesktopConfig{
+    public $confname;
 
+    public function __construct(){
+       $this -> confname = 'Game Configurations';
+       $this -> cm = new ConfigManager(null);
+    }
+
+
+    public function getConfigurations($type){
+        $config = $this->cm->getGameConfig();
+        return $config;
+    }
 }
 ?>
