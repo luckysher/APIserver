857aa865ad7e7ae49912b1a1981097eda6104049
luckysher <luckyshera95@gmail.com>
2018-05-02 17:27:58 +0530

    added not found urls

--- errorHandler.php
+++ errorHandler.php
@@ -10,11 +10,12 @@ class Error{
         $this-> status = 404;
         $this->$message = "URL not available";
         $this->$notFoundUrl->$notFoundUrl;
-        $this->$availableUrls; = [];
+        $this->$availableUrls; = ["localhost/PHPapi/api.php/getconfig?type=desktop", "localhost/PHPapi/api.php/getconfig?type=game"];
     }
 
     public function setNotFound($nUrl){
         $this->$notFoundUrl = $nUrl;
     }
+
 }
 ?>
