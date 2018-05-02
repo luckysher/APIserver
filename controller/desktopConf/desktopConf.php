7955a9480297d4c564ec7fe26f7f0e1a70c7fccf
luckysher <luckyshera95@gmail.com>
2018-05-02 16:53:51 +0530

    updated desktop config manager

--- controller/desktopConf/desktopConf.php
+++ controller/desktopConf/desktopConf.php
@@ -4,9 +4,11 @@ include 'config/configManager.php';
 
 class DesktopConfig{
     public $confname;
+    public $type;
 
     public function __construct(){
        $this -> confname = 'Desktop Configurations';
+       $this -> type = 'desktop';
        $this -> cm = new ConfigManager(null);
     }
 
