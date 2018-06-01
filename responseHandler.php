92678c9c47939900cae34b600911f77030e93d1e
luckysher <luckyshera95@gmail.com>
2018-06-01 17:45:25 +0530

    updated README

--- responseHandler.php
+++ responseHandler.php
@@ -4,32 +4,21 @@
 class ResponseHandler{
     public $status;
     public $message;
-    public $code;
-    public $availableUrls;
+
 
     // constructor
     public function __construct(){
-        $this-> code = 200;
-        $this-> status = "ok";
-        $this->$message = "ok";
-        $this->$availableUrls; = ["localhost/PHPapi/api.php/getconfig?type=desktop", "localhost/PHPapi/api.php/getconfig?type=game"];
+       $this->$availableUrls; = ["localhost/PHPapi/api.php/getconfig?type=desktop", "localhost/PHPapi/api.php/getconfig?type=game"];
     }
 
     // function for making a response message
     public function getMessage($message = NULL, $code = NULL, $status = NULL){
-        // Make a default message array
-        $resMesg = Array();
-        $resMesg["code"] = $this->code;
-        $resMesg["status"] = $this->status;
-        $resMesg["message"] = $this->message;
+
     }
 
     // get success response message on GET/POST request
     public function getSuccessResp($message, $dataArray=NULL){
-        $resMesg = $this->getMessage("success");
-        if($dataArray){
-            $resMesg["data"] = $dataArray;
-        }
+
         return $resMesg;
     }
 
